3d1a2791fbf77f485e40873cc5df062479cbf219
DnAp <dnlebedev@gmail.com>
2014-06-25 11:39:28 +0400

    Убрал лишнее свойство товара

--- install/index.php
+++ install/index.php
@@ -102,28 +102,6 @@ Class ddelivery extends CModule
                 'INPUT_FIELD_LOCATION' => '0',
                 'MULTIPLE' => 'N',
             ));
-            CSaleOrderProps::add(array (
-                'PERSON_TYPE_ID' => '1',
-                'NAME' => 'DDelivery ID',
-                'TYPE' => 'TEXT',
-                'REQUIED' => 'N',
-                'DEFAULT_VALUE' => '',
-                'SORT' => '10000',
-                'USER_PROPS' => 'N',
-                'IS_LOCATION' => 'N',
-                'PROPS_GROUP_ID' => '2',
-                'IS_EMAIL' => 'N',
-                'IS_PROFILE_NAME' => 'N',
-                'IS_PAYER' => 'N',
-                'IS_LOCATION4TAX' => 'N',
-                'IS_ZIP' => 'N',
-                'CODE' => 'DDELIVERY_REAL_ID',
-                'IS_FILTERED' => 'Y',
-                'ACTIVE' => 'Y',
-                'UTIL' => 'Y',
-                'INPUT_FIELD_LOCATION' => '0',
-                'MULTIPLE' => 'N',
-            ));
 
 
             $IntegratorShop = new DDeliveryShop($ddeliveryConfig['CONFIG']['CONFIG'], array(), array());
